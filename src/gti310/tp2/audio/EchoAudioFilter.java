171c87226a9f4dae4fc7d4bfb36e884754d15c18
Defoy <am01620@logti-a3412-15.ens.ad.etsmtl.ca>
2015-02-05 14:51:48 -0500

    start up du projet

---
+++ src/gti310/tp2/audio/EchoAudioFilter.java
@@ -0,0 +1,17 @@
+package gti310.tp2.audio;
+
+import gti310.tp2.io.FileSource;
+
+public class EchoAudioFilter implements AudioFilter {
+
+	public EchoAudioFilter(FileSource fs, int frequency, short bytePerSample){
+		
+	}
+	
+	@Override
+	public void process() {
+		
+		
+	}
+
+}
